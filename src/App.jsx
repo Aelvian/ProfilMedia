651db538b3fcb28092535b32ad0f87f3a59212d4
Aelvian <Aelvian22@gmail.com>
2025-10-15 11:50:01 +0700

    perbaikan tampilan responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/picture.jpg'
-import viteLogo from '/picture.jpg'
-// import './App.css';
+
+
 import Profil from './Picture/Picture.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
